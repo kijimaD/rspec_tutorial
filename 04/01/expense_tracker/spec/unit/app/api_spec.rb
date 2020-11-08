d7ca479dccd965508807bb4a41be8312652cf201
kijima <norimaking777@gmail.com>
2020-11-09 00:40:24 +0900

    Add Ledger

--- 04/01/expense_tracker/spec/unit/app/api_spec.rb
+++ 04/01/expense_tracker/spec/unit/app/api_spec.rb
@@ -2,8 +2,6 @@ require_relative '../../../app/api'
 require 'rack/test'
 
 module ExpenseTracker
-  RecordResult = Struct.new(:success?, :expense_id, :error_message)
-
   RSpec.describe API do
     include Rack::Test::Methods
 
@@ -35,11 +33,6 @@ module ExpenseTracker
           expect(last_response.status).to eq(200)
         end
 
-      end
-
-      context 'when the expense fails validation' do
-        it 'return an error message'
-        it 'responds with a 422 (Unprocessable entity'
       end
     end
 
